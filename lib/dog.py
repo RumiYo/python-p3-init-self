878f0d747d5d617444eca4bf81f1a32976fa5cb3
RumiYo <yokoyamarumi@gmail.com>
2024-02-29 00:31:27 -0500

    Complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        # print(f"{name} is born!")
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def showing_self(self):
+        return self
+
+fido = Dog("Fido")
+fido.name
+
+snoopy = Dog("Snoopy")
+snoopy.name
